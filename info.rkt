26c1dcebba94d91a524612b01cd89d4850d5a76c
unknown <stephen@thoughtstem.com>
2020-09-16 14:29:42 -0700

    dep

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "codespells")
-(define deps '("base" "aws"))
+(define deps '("base" "https://github.com/srfoster/codespells-server.git"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/codespells.scrbl" ())))
 (define pkg-desc "Description Here")

1ce4a3ffde33e87e361ac01bce72eff0c5da1366
Stephen Foster <stephen@thoughtstem.com>
2020-11-12 11:03:33 -0800

    more comments

--- lore/display.rkt
+++ lore/display.rkt
@@ -1,29 +1,30 @@
 #lang racket
 
-(require "./base.rkt" website-js)
+(require "./base.rkt" website-js
+	 website-js/components/accordion-cards)
 
 ;For display of lore things
+;  Note that many of these functions do not take
+;  lore structs as input.  Instead they take names
+;  of packages that have a lore module.
+;  e.g. 'fire-particles
+;  From this, it is easy to dynamically require
+;  'fire-particles/lore, and/or anything else that
+;  might be useful in the 'fire-particles package,
+;  (like the rune language, etc.)
 
-(provide link-to-collection
-         rune-collection-listing
-         rune-collection-name->preview-card 
-         rune-collection-name->preview-icon
-	 lore->name-slug)  
+(provide rune-collection-name->preview-card 
+	 rune-collection-name->preview-icon
+	 authored-work-name->preview-image/page 
+	 authored-work-name->authored-work-card
+	 rune-collection-name->rune-collection/page
+	 lore->name-slug
 
-(require syntax/parse/define
-	 website-js/components/accordion-cards
-	 (for-syntax racket/syntax
-		     racket/format))
+	 authored-work-card
+	 download-button
+	 coming-soon)
 
-(provide 
-  authored-work-lore->preview-image/page 
-  authored-work-lore->authored-work-card
-  rune-collection-lore->rune-collection/page
-
-  authored-work-card
-  download-button
-  coming-soon)
-
+;Useful for creating URLs
 (define (lore->name-slug lore)
   (string-replace 
     (string-downcase (cond 
@@ -33,9 +34,11 @@
 			(rune-collection-lore-name lore)]))
     " " "-"))
 
+;To standardize links that go to collections
 (define (link-to-collection name [content name])
   (a href: 
      ;The path convention on codespells.org
+     ;  Can always add a kw param if need something else.
      (~a "/collections/"
 	 (string-replace 
 	   (string-downcase name)
@@ -43,7 +46,10 @@
 	 "/index.html")
      content))
 
-(define (rune-collection-listing name preview)
+
+;Nice to have a card that contains a link to the
+;  Rune Collection's fulll page
+(define (rune-collection-preview-card name preview)
   (card 
     (card-header
       (link-to-collection name))
@@ -51,6 +57,7 @@
       preview)))
 
 
+;Generate a rune collection preview card from the name
 (define (rune-collection-name->preview-card module-name)
   (define module-name/lore (string->symbol
 			     (~a module-name "/lore")))
@@ -58,9 +65,13 @@
   (define lore 
     (dynamic-require module-name/lore 'lore))
 
-  (rune-collection-listing (rune-collection-lore-name lore) 
-			   (rune-collection-lore-preview-image lore)))
+  (rune-collection-preview-card 
+    (rune-collection-lore-name lore) 
+    (rune-collection-lore-preview-image lore)))
 
+
+;Sometimes you don't want a full card, just an icon
+;  that links to the collection's full page
 (define (rune-collection-name->preview-icon module-name)
   (define module-name/lore (string->symbol
 			     (~a module-name "/lore")))
@@ -72,8 +83,8 @@
 		      (rune-collection-lore-preview-image lore)))
 
 
-
-(define (rune-collection-lore->rune-collection/page
+;Constructs the full page that you can (render #:to "wherever")
+(define (rune-collection-name->rune-collection/page
 	  rune-collection-name
 	  #:path path
 	  #:wrapper[wrapper identity])
@@ -94,7 +105,10 @@
 	      (rune-collection-lore-rune-lores lore))))))
 
 
-(define (authored-work-lore->preview-image/page authored-work-name #:path path )
+;Creates the "page" you can (render #:to "out" ...), 
+;  but in this case "page" is the image file (e.g. the png).
+;  In `website` "page" just means file.
+(define (authored-work-name->preview-image/page authored-work-name #:path path )
   (let ()
     (define lore (dynamic-require-lore authored-work-name))
 
@@ -103,8 +117,11 @@
     (page (identity path)  
 	  preview-image)))
 
-
-(define (authored-work-lore->authored-work-card pkg-name )
+;A card for an authored work.  Currently,
+;  we don't have full pages for authored works.  But 
+;  we certainly might in the future, in which case
+;  the link to the page would be found here.
+(define (authored-work-name->authored-work-card pkg-name )
   (let ()
     (define lore (dynamic-require-lore pkg-name))
 
@@ -117,16 +134,16 @@
     (define preview-image
       (authored-work-lore-preview-image lore))
 
-    (authored-work-card (h3 name) 
-		(img class: "card-img-top"
+    (authored-work-card 
+      #:name (h3 name) 
+      #:preview-image (img class: "card-img-top"
 		     src: (~a 
 			    "works/" (lore->name-slug lore)
 			    "/preview.png"))
 
 		(accordion-card #:header "Read More..."
 				(h5 "Rune Collections")
-				(map 
-				  rune-collection-name->preview-icon 
+				(map rune-collection-name->preview-icon 
 				  rune-collections)
 				(br)
 				description)
@@ -137,7 +154,7 @@
 		(download-button (~a pkg-name)))))
 
 
-(define (authored-work-card title img . content)
+(define (authored-work-card #:name title #:preview-image img . content)
   (card
     (card-header title
 		 img)
